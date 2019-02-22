5babfb9901a972982a97d02d146fd37efd9e6800
madams102 <33868107+madams102@users.noreply.github.com>
2019-02-22 10:23:51 -0500

    Add files via upload

--- DecisionFactory.py
+++ DecisionFactory.py
@@ -1,22 +1,108 @@
 import random
 import numpy as np
 
+'''
+All functions related to memory will start with "m_"
+'''
+
 class DecisionFactory:
-    def __init__(self, name="DecisionFactory"):
-        self.name = name
-        self.directions = ['wait', 'up', 'down', 'right', 'left']
-        self.last_result = 'success'
-        self.last_direction = 'wait'
-        #self.state.pos = (0, 0)
+	def __init__(self, name='Walter Wanderley'):
+		self.name = name
+		self.directions = ['wait', 'up', 'down', 'right', 'left']
+		self.failed_directions = []
+		self.last_result = 'start'
+		self.last_direction = 'wait'
+                self.pos = [0, 0] #this is the players x,y coords in the 'memory'
+                self.memory = np.full((1, 1), '?')
+	#Note: we have relativistic coordinates recorded here, since the map
+	# is relative to the player's sfirst known and recorded position:
+	#self.state.pos = (0,0)
+        
+        def m_printMem(self):
+            for y in range(0, len(self.memory)):
+                    for x in range(0, len(self.memory[0])):
+                        print self.memory[y][x],
+                    print
 
-    def get_decision(self, verbose = True):
-        return self.random_direction()
+        def m_fixMatrix(self):
+            newMatrix = [[0 for y in range(len(self.memory))] for x in range(len(self.memory[0]))]
+            for y in range(len(self.memory)):
+                for x in range(len(self.memory[0])):
+                    newMatrix[x][y] = self.memory[y][x]
+            self.memory = newMatrix
 
-    def random_direction(self):
-        r = random.randint(1, 4)
-        self.last_direction = self.directions[r]
+        def m_extendArray(self, direction):
+                if direction == 'up':
+                    self.pos[0] -= 1
+                elif direction == 'down':
+                    self.pos[0] += 1
+                elif direction == 'left':
+                    self.pos[1] += 1
+                elif direction == 'right':
+                    self.pos[1] -= 1
 
-        return self.directions[r]
+                #check if player is in memories current bounds
+                if 0 >= self.pos[0]: #check left side
+                    self.pos[0] += 1
+                    self.memory = np.insert(self.memory, 0, '?', axis=0)
+                elif len(self.memory) < self.pos[0]: #check right side
+                    self.pos[0] -= 1
+                    self.memory = np.insert(self.memory, len(self.memory-1), '?', axis=0)
+                elif 0 >= self.pos[1]: #check top side
+                    self.pos[1] -= 1
+                    self.memory = np.insert(self.memory, 0, '?', axis=1)
+                elif len(self.memory[0]) < self.pos[1]: #check bottom side
+                    self.pos[1] += 1
+                    self.memory = np.insert(self.memory, len(self.memory[0]-1), '?', axis=1)
 
-    def put_result(self, result):
-        self.last_result = result
+#        def movePos(self, direction):
+
+	def get_decision(self, verbose = True):
+		# return self.random_direction()
+                self.m_fixMatrix()
+                print self.pos
+		self.m_printMem()
+                direction = self.better_than_random()
+                self.m_extendArray(direction)
+#                self.movePos()
+                return direction 
+
+
+	def random_direction(self):
+		#random.randint(0,4) #Includes wait state
+		r = random.randint(1,4) #Does NOT include wait state
+
+		return self.directions[r]
+
+	def better_than_random(self):
+		r = self.random_direction()
+
+		# print "Last result: ", self.last_result
+		# print "Last direction: ", self.last_direction
+
+		self.failed_directions.extend(self.last_direction)
+
+		if self.last_result != 'success':
+			self.failed_directions.extend(self.last_direction)
+
+			r = random.randint(1,4)
+
+			while self.directions[r] in self.failed_directions:
+				# print "Failed:", self.failed_directions
+				r = random.randint(1,4)
+
+			# print self.directions[r]
+			return self.directions[r]
+		
+		else:	
+			self.failed_directions = []
+
+
+			# r = self.last_direction
+			r = self.random_direction()
+			# print r
+			return r
+
+	def put_result(self, result):
+		self.result = result
+
--- framework.py
+++ framework.py
@@ -1,139 +1,274 @@
+'''
+	ALEC BOULWARE
+	MATT ADAMS
+	SEAN BRITTINGHAM
+
+	COSC 490
+	SPRING 2019
+	DECISION FACTORY AND FRAMEWORK
+'''
+
+'''
+	TODO: none!
+'''
+
 import pygame, sys, time
 from pygame.locals import *
 from DecisionFactory import *
-PURPLE = (189, 23, 173)
-BLACK = (0, 0, 0)
-GREEN = (0, 255, 0)
-GREY = (130, 130, 130)
+from inputMap import readMap
 
-GROUND = 0
-WALL = 1
-PORTAL = 2
-PLAYER = 3
+#declare tile colors
+PURPLE = (189, 23, 173) #portal
+BLACK = (0, 0, 0)       #ground/none
+GREEN = (0, 255, 0)     #player
+GREY = (130, 130, 130)  #wall
 
+#declare tile types
+F = 0
+W = 1
+G = 2
+P = 3
+N = 'x' #refers to a spot that cannot be spawned in,
+	#note that if a player walks over this spot, it will become F
+
+#assign colors
 colors = {
-		PLAYER : GREEN,
-		PORTAL : PURPLE,
-                GROUND : BLACK,
-                WALL : GREY
+		P : GREEN,
+		G : PURPLE,
+		F : BLACK,
+		W : GREY, 
+		N : BLACK
 	}
+#map for Walter to wander in
+tilemap = []
 
-tilemap = [
-            [GROUND, GROUND, GROUND, GROUND, GROUND, GROUND, GROUND, GROUND, GROUND, GROUND],
-            [GROUND, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, GROUND],
-            [GROUND, WALL, GROUND, GROUND, GROUND, GROUND, GROUND,GROUND, WALL, GROUND],
-            [GROUND, WALL, GROUND, GROUND, GROUND, GROUND, GROUND,GROUND, WALL, GROUND],
-            [GROUND, WALL, GROUND, GROUND, GROUND, GROUND, GROUND,GROUND, WALL, GROUND],
-            [GROUND, WALL, GROUND, GROUND, GROUND, GROUND, GROUND,GROUND, WALL, GROUND],
-            [GROUND, WALL, GROUND, GROUND, GROUND, GROUND, GROUND,GROUND, WALL, GROUND],
-            [GROUND, WALL, GROUND, GROUND, GROUND, GROUND, GROUND,GROUND, WALL, GROUND],
-            [GROUND, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, GROUND],
-            [GROUND, GROUND, GROUND, GROUND, GROUND, GROUND, GROUND, GROUND, GROUND, GROUND],
-	]
-
+#dimensions
 TILESIZE = 40
-MAPWIDTH = 10
-MAPHEIGHT = 10
+MAPWIDTH = 10		#default- used if no map file is passed at execution
+MAPHEIGHT = 10		#default- used if no map file is passed at execution
 
-pygame.init()
-DISPLAYSURF = pygame.display.set_mode((MAPWIDTH*TILESIZE, MAPHEIGHT*TILESIZE))
-
-df = DecisionFactory()
-#player position
+#define position globally
 position = (0, 0)
 
-def determineResult(decision):
-    dx = 0
-    dy = 0
-    if decision == 'up':
-        dy = -1
-    elif decision == 'down':
-        dy = 1
-    elif decision == 'left':
-        dx = -1
-    elif decision == 'right':
-        dx = 1
 
-    result = tilemap[position[0] + dx][position[1] + dy]
-    print "Original: (" + str(position[0]) + ", " + str(position[1]) + ")"
-    print "New:      (" + str(position[0] + dx) + ", " + str(position[1] + dy) + ")"
-    if result == WALL:
-        return 'wall'
-    elif result == GROUND:
-        return 'success'
-    elif result == PORTAL:
-        return 'foundPortal'
-    else:
-        return 'error'
 
+'''
+
+'''
 def initPlayerAndPortal():
-    #initPlayer
-    success = False
-    global position
-    while success == False:
-        rx = random.randint(1, MAPWIDTH - 1)
-        ry = random.randint(1, MAPHEIGHT - 1)
-        if tilemap[rx][ry] != WALL:
-            tilemap[rx][ry] = PLAYER
-            print rx
-            print ry
-            position = (rx, ry)
-            success = True
+	#check for initOverride
+	initOverride = False
+	if len(sys.argv) >= 3:
+		for i in range(len(sys.argv)):
+			if sys.argv[i] == "-initOverride" or sys.argv[i] == "-r":
+				initOverride = True
 
-    success = False
-    while success == False:
-        rx = random.randint(1, MAPWIDTH - 2)
-        ry = random.randint(1, MAPHEIGHT - 2)
-        if tilemap[rx][ry] != WALL and tilemap[rx][ry] != PLAYER:
-            tilemap[rx][ry] = PORTAL
-            success = True
+#check if player or portal already exists in textmap
+	playerExists = False
+	portalExists = False
+	global position
+	for y in range(0, MAPHEIGHT):
+		for x in range(0, MAPWIDTH):
+			if tilemap[x][y] == 3:
+				if initOverride == False:
+					playerExists = True
+					position = [x, y]
+				else:
+					tilemap[x][y] = 0
+			if tilemap[x][y] == 2:
+				if initOverride == False:
+					portalExists = True
+				else:
+					tilemap[x][y] = 0	
 
-def movePlayer(position, decision):
-    global tilemap
-    tilemap[position[0]][position[1]] = GROUND
-    if decision == 'up':
-        tilemap[position[0]][position[1] - 1] = PLAYER
-    elif decision == 'down':
-        tilemap[position[0]][position[1] + 1] = PLAYER
-    elif decision == 'left':
-        tilemap[position[0] - 1][position[1]] = PLAYER
-    elif decision == 'right':
-        tilemap[position[0] + 1][position[1]] = PLAYER
+	if playerExists == False:
+		#initPlayer
+		success = False
+		while success == False:
+			rx = random.randint(0, MAPWIDTH - 1)
+			ry = random.randint(0, MAPHEIGHT - 1)
 
+			if tilemap[rx][ry] != W and tilemap[rx][ry] != N:
+				tilemap[rx][ry] = P
+				position = [rx, ry]
+				success = True
+
+	if portalExists == False:
+		#init portal
+		success = False
+		while success == False:
+			rx = random.randint(1, MAPWIDTH - 2)
+			ry = random.randint(1, MAPHEIGHT - 2)
+			if tilemap[rx][ry] != W and tilemap[rx][ry] != P and tilemap[rx][ry] != N:
+				tilemap[rx][ry] = G
+				success = True
+
+'''
+	print map to std. out
+'''
 def printTilemap():
-    for x in range(0, MAPHEIGHT):
-        for y in range(0, MAPWIDTH):
-            print tilemap[x][y],
-        print
+	for y in range(0, MAPHEIGHT):
+		for x in range(0, MAPWIDTH):
+			print tilemap[x][y],
+		print
 
-initPlayerAndPortal()
-steps = 0
+'''
 
-while True:
-	for event in pygame.event.get():
-		if event.type == QUIT:
+'''
+def determineResult(decision):
+	d_ver = 0 #d_ver? I hardly know her! LOL my bad i meant to change it back, x/y were confusing me atm. - In a world where x=y, who can blame thee?
+	d_hor = 0 
+	if decision == 'up':
+		d_ver = -1
+	elif decision == 'down':
+		d_ver = 1
+	elif decision == 'left':
+		d_hor = -1
+	elif decision == 'right':
+		d_hor = 1
+
+	global position
+	result = tilemap[position[0] + d_hor][position[1] + d_ver]
+	print "Starting:", position
+	trying = (position[0] + d_hor, position[1] + d_ver)
+	print "Trying:", trying
+  
+	if result == W:
+		return 'wall'
+	elif result == F:
+		return 'success'
+	elif result == N:
+		return 'success'
+	elif result == G:
+		return 'foundPortal'
+	else:
+		return 'error'
+
+'''
+.
+'''
+def movePlayer(position, decision):
+	global tilemap
+
+	old_x = position[0]
+	old_y = position[1]
+
+	tilemap[position[0]][position[1]] = F
+	
+	if decision == 'up':
+		tilemap[position[0]][position[1] - 1] = P
+		position[1] = position[1]-1
+	elif decision == 'down':
+		tilemap[position[0]][position[1] + 1] = P
+		position[1] = position[1]+1
+	elif decision == 'left':
+		tilemap[position[0] - 1][position[1]] = P
+		position[0] = position[0]-1
+	elif decision == 'right':
+		tilemap[position[0] + 1][position[1]] = P
+		position[0] = position[0]+1
+
+'''
+Fixes Matrix so that y!=x. Ask no questions, just take it for granted,
+I don't even understand.
+'''
+def fixMatrix(matrix):
+    newMatrix = [[0 for y in range(MAPHEIGHT)] for x in range(MAPWIDTH)]
+    for y in range(MAPHEIGHT):
+        for x in range(MAPWIDTH):
+            newMatrix[x][y] = matrix[y][x]
+    global tilemap
+    tilemap = newMatrix
+
+def main():
+	if len(sys.argv) >= 2: #reads file name, ignores all other arguments passed
+		if sys.argv[1] == "-noGraphics" or sys.argv[1] == "-ng":
+			map_file = "map00.txt"
+		elif sys.argv[1] == "-initOverride" or sys.argv[1] == "-r":
+			map_file = "map00.txt"	
+		elif sys.argv[1] == "-fast" or sys.argv[1] == "-f":
+			map_file = "map00.txt"
+		else:
+			map_file = sys.argv[1]
+	else:
+		map_file = "map00.txt"
+
+	global MAPHEIGHT
+	global MAPWIDTH
+	global tilemap
+
+	map_info = readMap(map_file)
+	MAPHEIGHT = map_info[0][1]
+	MAPWIDTH = map_info[0][0]
+	tilemap = map_info[1]
+        
+        #swap tiles to match the input map
+        fixMatrix(tilemap)
+
+	#turn off map printing w/ -noGraphics
+	noGraphics = False
+	if len(sys.argv) >= 2:
+		for i in range(len(sys.argv)):
+			if sys.argv[i] == "-noGraphics" or sys.argv[i] == "-ng":
+				noGraphics = True
+	if noGraphics == False:
+		#initialize the display
+		pygame.init()
+		pygame.display.set_caption("Walter Wanderley")
+       		DISPLAYSURF = pygame.display.set_mode((MAPWIDTH*TILESIZE, MAPHEIGHT*TILESIZE))
+
+	initPlayerAndPortal()
+	steps = 0               #steps to find goal
+	df = DecisionFactory()  #initialize DecisionFactory
+	
+	fast = False
+	if len(sys.argv) >= 2:
+		for i in range(len(sys.argv)):
+			if sys.argv[i] == "-fast" or sys.argv[i] == "-f":
+				fast = True
+	printTilemap()
+	while True:
+		if noGraphics == False:
+			for event in pygame.event.get():
+				if event.type == QUIT:
+					pygame.quit()
+					sys.exit()
+		
+		if fast == False:		
+			time.sleep(0.08)
+		print
+
+		decision = df.get_decision()
+		print "Decision: ", decision
+		#get result of 'walk'
+		result = determineResult(decision) 
+		print "Result: ", result
+
+		if result == 'foundPortal':
+			print "Found portal in", steps, "steps!\n"
+			df.put_result('success')
 			pygame.quit()
 			sys.exit()
-                
-        time.sleep(0.7)
-        printTilemap()
-        decision = df.get_decision()
-        #get result of 'walk'
-        result = determineResult(decision) 
-        print result
-        if result == 'foundPortal':
-            print "Found portal in " + str(steps) + " steps!\n"
-            df.put_result('success')
-            pygame.quit()
-            sys.exit()
-        else:
-            df.put_result(result)
-        print decision
-        if result == 'success':
-            movePlayer(position, decision)
-	for row in range(MAPHEIGHT):
-		for column in range(MAPWIDTH):
-			pygame.draw.rect(DISPLAYSURF, colors[tilemap[row][column]], (column*TILESIZE, row*TILESIZE, TILESIZE, TILESIZE))
-        steps += 1
-	pygame.display.update()
+		else:
+			df.put_result(result)
+		steps += 1
+                print "Steps: ", steps
+		if result == 'success':
+			movePlayer(position, decision)
 
+	    	printTilemap()
+		
+		if noGraphics == False:
+			#draw map to screen	
+			for column in range(MAPWIDTH):
+				for row in range(MAPHEIGHT):
+					pygame.draw.rect(DISPLAYSURF, colors[tilemap[column][row]], (column*TILESIZE, row*TILESIZE, TILESIZE, TILESIZE))
+			
+			pygame.display.update()
+
+
+'''
+	Flow control
+'''
+if __name__ == "__main__":
+	main()
